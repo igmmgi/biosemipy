542912df735dab6351c2606ef32f5043b83be01a
igmmgi <ian.mackenzie@uni-tuebingen.de>
2019-04-11 13:08:35 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='biosemipy',
-      version='0.0.1',
+      version='0.0.2',
       description='Code to read BioSemi BDF EEG files',
       author='IGM',
       packages=['biosemipy'],
       license='MIT',
-      install_requires=['numba', 'numpy'],
+      install_requires=['numba', 'numpy', 'pyqtgraph', 'PyQt5', 'matplotlib'],
       zip_safe=False
       )
